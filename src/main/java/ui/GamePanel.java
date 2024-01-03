a5691bb9eda42dfed4892331200f635bc244d6b4
罗铃皓 <1185303787@qq.com>
2024-01-03 19:45:41 +0800

    界面微调

--- src/main/java/ui/GamePanel.java
+++ src/main/java/ui/GamePanel.java
@@ -49,8 +49,8 @@ public class GamePanel extends JPanel {
         g.drawImage(leftboundImage, 0, 0, 24, this.getHeight(), this);
         g.drawImage(rightboundImage, this.getWidth()-264, 0, 24, this.getHeight(), this);
         g.drawImage(verticalBoundImage,this.getWidth()-24,0,24,this.getHeight(),this);
-        g.drawImage(horizonBoundImage,24 * 24,0,24 * 9,22,this);
-        g.drawImage(horizonBoundImage,24 * 24,24 * 22,24 * 9,22,this);
+        g.drawImage(horizonBoundImage,582,0,219,22,this);
+        g.drawImage(horizonBoundImage,582,24 * 22,219,22,this);
         g.drawImage(horizonBoundImage,24 * 24,this.getHeight() - 22,24 * 9,22,this);
         //绘制
         for (IDraw draw : this.draws) {
